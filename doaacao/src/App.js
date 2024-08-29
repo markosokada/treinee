a2e381b55685fc3dec13b974e57c397a2cd984a1
markosokada <markosokada@gmail.com>
2024-08-29 08:30:36 -0300

    resolvendo conflitos de mesclagem

--- doaacao/src/App.js
+++ doaacao/src/App.js
@@ -1,8 +1,11 @@
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
-import Home from './Pages/Home';
-import SignIN from './Pages/SignIn';
-import SignUp from './Pages/SignUp';
-import Doacao from './Pages/Doacao';
+
+import Home from './Components/Pages/Home';
+import Projects from './Components/Pages/Projects';
+import SignIN from './Components/Pages/SignIn';
+import SignUp from './Components/Pages/SignUp';
+import Doacao from './Components/Pages/Doacao';
+import NewProject from './Components/Pages/NewProject';
 
 import Container from './Components/Layout/Container';
 import NavBar from './Components/Layout/NavBar';
@@ -12,14 +15,16 @@ function App() {
   return (
     <Router>
       <NavBar />
-      <Container customClass="min-height" />
+      <Container customClass="min-height">
       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/SignIN" element={<SignIN />} />
-          <Route path="/SignUp" element={<SignUp />} />
-          <Route path="/Doacao" element={<Doacao />} />
-          <Route path="/NovaCampanha" element={<NovaCampanha />} />
-        </Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Projects" element={<Projects />} />
+        <Route path="/SignIN" element={<SignIN />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/Doacao" element={<Doacao />} />
+        <Route path="/NewProject" element={<NewProject />} />
+      </Routes>
+
       </Container>
       <Footer />
     </Router>
